70bfd0de295fa4d9fb02f903bf6066ddabb55e26
elenam <elenam@morris.umn.edu>
2023-02-13 12:29:44 -0600

    added multi-arity and variadic functions

--- csci4651/src/csci4651/core.clj
+++ csci4651/src/csci4651/core.clj
@@ -11,4 +11,30 @@
         y (if (not (empty? (rest coll))) (second coll) 0)]
         (+ x y)))
 
-(println (intro-let [1 2 3]))
+;(println (intro-let [1 2 3]))
+
+;; Using destructuring:
+(defn destr1 [[x y & more]]
+      (+ x y)) ; note: this will break if there's fewer than two items in the collection
+
+(defn destr2 [[x y & more]]
+      (+ (or x 0) (or y 0))) ; why does it work?
+
+;; multi-arity functions:
+(defn greeting
+  ([first-name] (str "Hello, " first-name))
+  ([first-name last-name] (str "Hello, " first-name " " last-name)))
+
+;; Use of apply:
+(+ 3 4 5) ; works
+;(+ [3 4 5]) ; doesn't
+;; If you are given a collection of items, you can use apply:
+(apply + [3 4 5]) ; same as (+ 3 4 5)
+
+;; variadic functions (functions with variable number of arguments)
+
+(defn sum
+  "Takes any number of arguments and adds them together"
+  ([] 0)
+  ([x] x)
+  ([x & xs] (+ x (apply sum xs)))) ; note implicit recursion, although not tail-recursion
--- csci4651/test/csci4651/core_test.clj
+++ csci4651/test/csci4651/core_test.clj
@@ -2,8 +2,35 @@
   (:require [clojure.test :refer :all]
             [csci4651.core :refer :all]))
 
+;; See https://clojuredocs.org/clojure.test/deftest for deftest macro
+;; description
+
 (deftest let-test
   (testing "Testing intro-let"
     (is (intro-let [1 2 3]) 3)
+    (is (intro-let '(1 2 3)) 3)
     (is (intro-let [5]) 5)
     (is (intro-let []) 0)))
+
+(deftest destr1-test
+  (testing "Testing the first version of destructuring"
+    (is (destr1 [1 2 3]) 3)
+    (is (destr1 '(1 2 3)) 3)))
+
+(deftest destr2-test
+  (testing "Testing the second version of destructuring"
+    (is (destr2 [1 2 3]) 3)
+    (is (destr2 '(1 2 3)) 3)
+    (is (destr2 []) 0)
+    (is (destr2 [1]) 1)))
+
+(deftest greeting-test
+  (testing "Testing the greeting function"
+    (is (greeting "Jane") "Hello, Jane")
+    (is (greeting "Jane" "Smith") "Hello, Jane Smith")))
+
+(deftest sum-test
+  (testing "Testing the sum function"
+    (is (sum) 0)
+    (is (sum 2) 2)
+    (is (sum 1 2 3 4) 10)))
