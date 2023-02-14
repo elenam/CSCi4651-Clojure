24354031b05495c3c9e534598bdaed26b5436617
elenam <elenam@morris.umn.edu>
2023-02-13 20:00:20 -0600

    fixed a doc string

--- csci4651/src/csci4651/core.clj
+++ csci4651/src/csci4651/core.clj
@@ -4,12 +4,13 @@
   (println "Hi there"))
 
 ;; Example of using let:
-(defn intro-let [coll]
+(defn intro-let
   "The function gives an example of using let. It takes a collection of numbers
    and returns the sum of the first two elements."
-  (let [x (if (not (empty? coll)) (first coll) 0)
-        y (if (not (empty? (rest coll))) (second coll) 0)]
-        (+ x y)))
+   [coll]
+   (let [x (if (not (empty? coll)) (first coll) 0)
+         y (if (not (empty? (rest coll))) (second coll) 0)]
+         (+ x y)))
 
 ;(println (intro-let [1 2 3]))
 
